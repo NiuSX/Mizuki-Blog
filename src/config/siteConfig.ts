8da08d774f9500c8876c38e907347d03b6fa79aa
NiuShaoXiong <1621980044@qq.com>
2026-09-12 21:45:24 +0800

    20260912

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,7 +6,7 @@ const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 export const siteConfig: SiteConfig = {
 	title: "一只捡星星的熊",
 	subtitle: "个人技术博客",
-	siteURL: "http://niushaoxiong.top/blog/", // 请替换为你的站点URL，以斜杠结尾
+	siteURL: "https://niushaoxiong.top/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2025-09-08", // 站点开始运行日期，用于站点统计组件计算运行天数
 	timeZone: "Asia/Shanghai", // 文章日期使用的 IANA 时区，可改为 Asia/Tokyo、Europe/Berlin 等
 
